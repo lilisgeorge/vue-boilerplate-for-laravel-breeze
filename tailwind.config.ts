90b29e97536373c270f7cd72ed2c2d6bf5bc162d
George Lilis <dev.george.lilis@gmail.com>
2023-03-29 20:26:59 +0300

    replace tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
-};
+} satisfies Config;
